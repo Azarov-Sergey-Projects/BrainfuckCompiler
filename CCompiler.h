06ca540656b99be39224bc78f683bbeb74b315fd
sergeyazarov1006@gmail.com <sergeyazarov1006@gmail.com>
2021-08-29 09:07:44 +0300

    Start Implementing class CCompiler

--- CCompiler.h
+++ CCompiler.h
@@ -1,7 +1,13 @@
 #pragma once
-
+#include <string>
+#include <array>
 
 class CCompiler
 {
-
+public:
+	CCompiler();
+	void readInstructionsFromFile(const std::string& fileName);
+	void interpretFunctions();
+private:
+	std::array<char,30000> commands;
 };
